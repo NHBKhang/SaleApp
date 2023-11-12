9273717db07062d81dc474341356672a3127048c
NHBKhang <nhbkhang12@gmail.com>
2023-11-12 18:10:57 +0700

    new

--- app/models.py
+++ app/models.py
@@ -24,4 +24,22 @@ class Product(db.Model):
 
 if __name__ == '__main__':
     with app.app_context():
-        pass
+        db.create_all()
+
+        db.session.add_all([Category(name='Mobile'), Category(name='Tablet'), Category(name='Desktop')])
+        db.session.commit()
+        #
+        # p1 = Product(name='IPhone13', price=20000000, category_id=1,
+        #              image='https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg')
+        # p2 = Product(name='Galaxy S23 Plus', price=22000000, category_id=1,
+        #              image='https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg')
+        # p3 = Product(name='iPad Pro 2023', price=35000000, category_id=2,
+        #              image='https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg')
+        # p4 = Product(name='Galaxy Tab S9', price=24000000, category_id=2,
+        #              image='https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg')
+        # p5 = Product(name='Note 23', price=20000000, category_id=1,
+        #              image='https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg')
+        # db.session.add_all([p1, p2, p3, p4, p5])
+        # db.session.commit()
+
+
